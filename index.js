2aaf3c3ef18beb9bb84aeab3ea0581d496726cd3
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-12-04 21:41:19 -0200

    fix errors with speed

--- index.js
+++ index.js
@@ -10,26 +10,28 @@ const cli = new meow([
 ])
 
 // check for updates
-updateNotifier({ pkg: cli.pkg }).notify()
+updateNotifier({ pkg: cli.pkg })  .notify()
 
-let st = speedTest({ maxTime: 15000 })
+let st = speedTest({ maxTime: 20000 })
 
 st.on('data', data => {
-  let download = data.speeds.download
-  let upload = data.speeds.upload
+  let download = (data.speeds.download * 100).toFixed(2)
+  let upload = (data.speeds.upload * 100).toFixed(2)
+  let ping = data.server.ping
 
-  console.log('Download speed: ' + download + 'kB/s')
-  console.log('Upload speed: ' + upload + 'kB/s')
+  console.log('\nFinal results:')
+  console.log('Download speed: ' + download + ' kB/s')
+  console.log('Upload speed: ' + upload + ' kB/s')
+  console.log('Latency: ' + ping + ' ms')
 
-  console.log({data})
+  // console.log({data})
 })
 
-st.on('downloadprogress', progress => {
-  console.log('Download progress -> ' + progress + '%')
+st.on('downloadspeedprogress', speed => {
+  console.log('Download: ' + (speed * 100).toFixed(2) + ' kB/s')
 })
-
-st.on('uploadprogress', progress => {
-  console.log('Upload progress -> ' + progress + '%')
+st.on('uploadspeedprogress', speed => {
+  console.log('Upload: ' + (speed * 100).toFixed(2) + ' kB/s')
 })
 
 st.on('error', err => {
