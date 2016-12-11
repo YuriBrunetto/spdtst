866b29d526088a212d4d83f476b5da8ad10ca5e7
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-12-11 11:39:09 -0200

    fix 100 to 125 (megabits)

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const cli = new meow([
 let st = speedTest({ maxTime: 20000 })
 
 st.on('data', data => {
-  let download = (data.speeds.download * 100).toFixed(2)
-  let upload = (data.speeds.upload * 100).toFixed(2)
+  let download = (data.speeds.download * 125).toFixed(2)
+  let upload = (data.speeds.upload * 125).toFixed(2)
   let ping = data.server.ping
 
   console.log('\nFinal results:')
@@ -25,10 +25,10 @@ st.on('data', data => {
 })
 
 st.on('downloadspeedprogress', speed => {
-  console.log('Download: ' + (speed * 100).toFixed(2) + ' kB/s')
+  console.log('Download: ' + (speed * 125).toFixed(2) + ' kB/s')
 })
 st.on('uploadspeedprogress', speed => {
-  console.log('Upload: ' + (speed * 100).toFixed(2) + ' kB/s')
+  console.log('Upload: ' + (speed * 125).toFixed(2) + ' kB/s')
 })
 
 st.on('error', err => {
